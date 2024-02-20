32649ecc2d1e136425451e08e2cd3fd430b696dc
LuckyShuii <lucasnimes30000@gmail.com>
2024-02-20 15:38:08 +0100

    front contact page

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('footer', './assets/footer.js')
     .addEntry('header', './assets/header.js')
+    .addEntry('contact_us', './assets/contact_us.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
