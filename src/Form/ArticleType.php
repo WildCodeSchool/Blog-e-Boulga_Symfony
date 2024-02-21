c7b7a69216976ff704d1ab30f54951fbad83290d
Mono <quentinguillemineau@gmail.com>
2024-02-21 15:38:44 +0100

    first version of article admin without front

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -16,6 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ArticleType extends AbstractType
 {
@@ -32,39 +33,74 @@ class ArticleType extends AbstractType
                 'label' => 'Titre',
                 'required' => true,
                 'help' => 'Le titre de votre article.',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le titre de votre article',
+                    ]),
+                ]
             ])
             ->add('homeTitle', TextType::class, [
                 'label' => 'Titre homepage',
                 'required' => true,
                 'help' => 'Le titre de votre article qui sera visible sur la homepage.',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le titre de votre article pour la homepage',
+                    ]),
+                ]
             ])
             ->add('homePreview', TextareaType::class, [
                 'label' => 'Description Homepage',
                 'required' => true,
                 'help' => 'Le texte de présentation présent sur la homepage.',
-                ])
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner la description pour la homepage',
+                    ]),
+                ]
+            ])
             ->add('description', TextareaType::class, [
                 'required' => true,
                 'help' => 'Rapide description résumant le sujet de votre article.',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner la description de votre article',
+                    ]),
+                ]
             ])
             ->add('introduction', TextareaType::class, [
                 'label' => 'Introduction',
                 'required' => true,
                 'help' => 'Le texte d\'introduction de votre article.',
-                ])
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le texte d\'introduction',
+                    ]),
+                ]
+            ])
             ->add('detail', TextareaType::class, [
                 'required' => true,
                 'help' => 'Le corps de votre article.',
-                ])
-
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le corps de votre article',
+                    ]),
+                ]
+            ])
             ->add('status', ChoiceType::class, [
                 'label' => 'Statut',
                 'help' => 'Choix du statut de votre article.',
                 'required' => true,
+
                 'choices' => [
                     'Brouillon' => '1',
                     'Publié' => '2',
                     'Archivé' => '3'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner le statut de votre article',
+                    ]),
                 ]
             ])
             ->add('author', EntityType::class, [
@@ -76,19 +112,33 @@ class ArticleType extends AbstractType
                 'choice_label' => function (Author $author) {
                     return $author->getUser()->getFirstName() . ' ' . $author->getUser()->getLastName();
                 },
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner l\'auteur de votre article',
+                    ]),
+                ]
             ])
             ->add('category', EntityType::class, [
                 'label' => 'Catégorie de l\'article',
                 'class' => Category::class,
                 'required' => true,
                 'choice_label' => 'category_name',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner la categorie de votre article',
+                    ]),
+                ]
             ])
             ->add('imgSrc', TextType::class, [
                 'label' => 'Choix de l\'image',
                 'required' => true,
                 'help' => 'L\'image qui illustrera votre article.',
-            ])
-            ;
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner l\'image de votre article',
+                    ]),
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
