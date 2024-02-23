9b09ed1bb37abd6d55fc60593706f95a6b3a7b78
LuckyShuii <lucasnimes30000@gmail.com>
2024-02-23 12:32:56 +0100

    hotfix demo home page + admin rouvoters

--- src/Controller/AdminArticleController.php
+++ src/Controller/AdminArticleController.php
@@ -13,8 +13,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/article')]
+#[IsGranted('VOTER_ADMIN', statusCode: 403, message: 'Vous n\'avez pas les droits pour accéder à cette page')]
 class AdminArticleController extends AbstractController
 {
     #[Route('/index/{status}', name: 'app_admin_article_index', methods: ['GET'], defaults: ['status' => null])]
